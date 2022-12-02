8026f984351d20ea2dc2516226723d7e76775a1c
Mariagoretti Kimani <mgmkimani@gmail.com>
2022-12-02 08:51:00 +0300

    add

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // Your code goes here
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.getElementById('text');
-    text.innerHTML = "This is really cool!"
-    console.log("The DOM has loaded");
-  });
+document.addEventListener("DOMContentLoaded", ()=>{
+  const element = document.getElementById("text");
+  element.innerHTML = "This is really cool!";
 
-  console.log(
-    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-  );
+});
